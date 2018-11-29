11bb60a0f1ba02c3415e2d30a72e417b2bb362a0
ruixuanHe <ruixuanheweb@gmail.com>
2018-11-28 23:56:22 -0500

    a

---
+++ cart211A3/quizJS.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function yes(){
+	document.getElementById("s2").style.transform = "translate(0%)"; 
+}
+function no(){
+	document.getElementById("s3").style.transform = "translate(0%)";
+}
+function next(){
+	document.getElementById("s2").style.transform = "translate(-100%)";
+	document.getElementById("s3").style.transform = "translate(100%)";
+}
---
+++ cart211A3/source/jquery-quiz-plugin/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jchamill/pen/garoqg.
+
+ A simple example of my jQuery quiz plugin.
